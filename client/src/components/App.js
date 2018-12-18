4137ee01b0f5eda545c9348e0eaea35f9c0ecda4
Agey <pzyktzle@gmail.com>
2018-12-18 17:28:57 +1000

    Setup React

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Hi There!
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => {
+  return <div>Hi There!</div>;
+};
+
+export default App;
